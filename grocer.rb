ecb445c01d80cea1ff623dc8c5775e99523117ff
Neal JIn Oh <no4259@gmail.com>
2019-08-16 20:06:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
-  # code here
+  def consolidate_cart(cart)
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
+end
 end
 
 def apply_coupons(cart, coupons)
